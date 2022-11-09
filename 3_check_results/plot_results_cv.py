ba31749dbaa4cccea22f9fafa26ae07f9d2a2b02
Nieto Nicolas <nnieto@sinc.unl.edu.ar>
2022-11-09 16:25:53 +0100

    set coef1 to rvc

---
+++ 3_check_results/plot_results_cv.py
@@ -0,0 +1,71 @@
+#%%
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import sys
+__file__ = '/home/nnieto/Nico/Harmonization/harmonize_project/3_check_results/plot_results_cv.py'
+to_append = Path(__file__).resolve().parent.parent.as_posix()
+sys.path.append(to_append)
+
+from lib.utils import table_generation, plot_barplot, extract_experiment_data , plot_grup_barplot
+from lib.utils import classification_table
+
+exp_dir = "/home/nnieto/Nico/Harmonization/results_r8"
+experiments_to_check = {
+    'test_all_regression_r8'
+}
+
+results_df = extract_experiment_data(exp_dir,experiments_to_check)
+plot_grup_barplot(results_df, True, True)
+plot_grup_barplot(results_df, True, False)
+
+plot_barplot(results_df, True, False)
+plot_barplot(results_df, True, True)
+
+table = table_generation(results_df)
+print(table)
+
+
+# %%
+
+exp_dir = "/home/nnieto/Nico/Harmonization/results"
+experiments_to_check = {
+    'test_all_regression'
+}
+
+results_df = extract_experiment_data(exp_dir,experiments_to_check)
+table = table_generation(results_df)
+print(table)
+
+
+
+plot_grup_barplot(results_df, True, True)
+plot_grup_barplot(results_df, True, False)
+
+plot_barplot(results_df, True, False)
+plot_barplot(results_df, True, True)
+
+# %%
+
+
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_all_classification'
+}
+
+results_df = extract_experiment_data(exp_dir,experiments_to_check)
+
+classification_table(results_df)
+
+#table = table_generation(results_df)
+#print(table)
+
+
+
+plot_grup_barplot(results_df, exlude_notarget = True, absolute_error = True, acc_bool= True)
+#plot_grup_barplot(results_df, True, False)
+
+#plot_barplot(results_df, True, False)
+#plot_barplot(results_df, True, True)
+
+# %%
---
+++ 3_check_results/plot_results_oos.py
@@ -0,0 +1,54 @@
+#%%
+from pathlib import Path
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+import sys
+__file__ = '/home/nnieto/Nico/Harmonization/harmonize_project/3_check_results/plot_results_oos.py'
+
+to_append = Path(__file__).resolve().parent.parent.as_posix()
+sys.path.append(to_append)
+
+from lib.utils import table_generation, plot_barplot, extract_experiment_data
+
+exp_dir = "/home/nnieto/Nico/Harmonization/results_r8"
+
+experiments_to_check = {'test_all_big_oos_ID1000_r8'}
+results_exp_ID = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_ID["site"] = "ID1000"
+
+experiments_to_check = {'test_all_big_oos_CamCAN_r8'}
+results_exp_CamCAN = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_CamCAN["site"] = "CamCAN"
+
+experiments_to_check = {'test_all_big_oos_1000Gehirne_r8'}
+results_exp_1000 = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_1000["site"] = "1000Gehirne"
+
+experiments_to_check = {'test_all_big_oos_eNKI_r8'}
+results_exp_eNKI = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_eNKI["site"] = "eNKI"
+
+results_df = pd.concat([results_exp_ID,results_exp_CamCAN,results_exp_1000,results_exp_eNKI])
+
+plot_barplot(results_df, False)
+table = table_generation(results_df)
+print(table)
+
+#%%
+import seaborn as sns
+sns.color_palette("pastel")
+import matplotlib.pyplot as plt
+
+results_df["y_diff"] = np.abs(results_df["y_true"]-results_df["y_pred"])
+
+g = sns.catplot(
+    data=results_df, kind= "boxen",
+    x="site", y="y_diff", hue="harmonize_mode", 
+    height=6
+)
+g.set_axis_labels("", "Prediction difference")
+g.legend.set_title("OOS experiment R8")
+plt.grid(alpha=0.5,axis="y", c="black")
+# %%
