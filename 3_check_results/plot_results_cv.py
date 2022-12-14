7d72b14aad4b80c51172d411030fe2a92503acb3
Nieto Nicolas <nnieto@sinc.unl.edu.ar>
2022-12-14 18:52:01 -0300

    return probability

--- 3_check_results/plot_results_cv.py
+++ 3_check_results/plot_results_cv.py
@@ -1,30 +1,80 @@
-#%%
+# %%
 import numpy as np
 import pandas as pd
 from pathlib import Path
 import sys
-__file__ = '/home/nnieto/Nico/Harmonization/harmonize_project/3_check_results/plot_results_cv.py'
+from sklearn.metrics import (
+    mean_absolute_error,
+)
+import matplotlib.pyplot as plt
+import seaborn as sbn
+
+dir_path = '/home/nnieto/Nico/Harmonization/harmonize_project/3_check_results/'
+__file__ = dir_path+'plot_results_cv.py'
 to_append = Path(__file__).resolve().parent.parent.as_posix()
 sys.path.append(to_append)
 
-from lib.utils import table_generation, plot_barplot, extract_experiment_data , plot_grup_barplot
-from lib.utils import classification_table
-#%%
+from lib.utils import table_generation, plot_barplot, extract_experiment_data
+from lib.utils import classification_table, plot_grup_barplot
+
+# %%
 exp_dir = "/home/nnieto/Nico/Harmonization/results_r8"
 experiments_to_check = {
     'test_all_regression_r8'
 }
 
-results_df = extract_experiment_data(exp_dir,experiments_to_check)
-plot_grup_barplot(results_df, True, True)
-plot_grup_barplot(results_df, True, False)
+results_df = extract_experiment_data(exp_dir, experiments_to_check)
+# plot_grup_barplot(results_df, True, True)
 
+data = results_df
+data["harmonize_mode"].replace({"pretend": "JuHarmonize",
+                                "target": "Traditional",
+                                "none": "None"}, inplace=True)
+
+harm_modes = ["JuHarmonize", "Traditional", "None"]
+
+if harm_modes is None:
+    harm_modes = np.unique(data["harmonize_mode"]).tolist()
+
+
+final_stat = []
+for mode in harm_modes:
+    resut_mode = data[data["harmonize_mode"] == mode]
+    predicted_age = resut_mode["y_pred"]
+    true_age = resut_mode["y_true"]
+    error_data = mean_absolute_error(true_age, np.round(predicted_age))
+    final_stat = np.append(final_stat, error_data)
+
+to_sort = [harm_modes, final_stat]
+df = pd.DataFrame(to_sort, index=["method", "value"])
+df = df.sort_values(by="value", axis=1)
+df = df.T
+sort_mode = df["method"]
+
+
+data["y_diff"] = data["y_true"]-data["y_pred"]
+
+g = sbn.catplot(
+    data=data, kind="boxen",
+    x="site", y="y_diff", hue="harmonize_mode",
+    height=10, hue_order=sort_mode, legend_out=True
+)
+g.set_axis_labels("", "Age prediction difference [years]")
+g.legend.set_title("Harmonization Schemes")
+sbn.move_legend(g, "upper right", bbox_to_anchor=(0.90, 0.98), frameon=False)
+
+plt.grid(alpha=0.5, axis="y", c="black")
+
+# %%
 plot_barplot(results_df, True, False)
 plot_barplot(results_df, True, True)
 
 table = table_generation(results_df)
 print(table)
-
+# %%
+plot_grup_barplot(results_df, False, False, ["pretend", "predict",
+                                             "predict_pretend",
+                                             "cheat", "target", "none"])
 
 # %%
 
@@ -33,29 +83,29 @@ experiments_to_check = {
     'test_all_regression'
 }
 
-results_df = extract_experiment_data(exp_dir,experiments_to_check)
+results_df = extract_experiment_data(exp_dir, experiments_to_check)
 table = table_generation(results_df)
 print(table)
 
-
-
 plot_grup_barplot(results_df, True, True)
-plot_grup_barplot(results_df, True, False)
+plot_grup_barplot(results_df, True, False, ["pretend", "cheat",
+                                            "target", "none"])
 
 plot_barplot(results_df, True, False)
 plot_barplot(results_df, True, True)
 
 # %%
 
-
 exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
 experiments_to_check = {
     'test_all_classification'
 }
 
-results_df = extract_experiment_data(exp_dir,experiments_to_check)
+results_df = extract_experiment_data(exp_dir, experiments_to_check)
 
-classification_table(results_df)
+classification_table(results_df, ["pretend", "predict",
+                                  "predict_pretend", "cheat",
+                                  "target", "none"])
 
 
 # %%
@@ -65,7 +115,7 @@ experiments_to_check = {
     'test_all_classification_RVC'
 }
 
-results_df = extract_experiment_data(exp_dir,experiments_to_check)
+results_df = extract_experiment_data(exp_dir, experiments_to_check)
 
 classification_table(results_df)
 # %%
@@ -75,7 +125,7 @@ experiments_to_check = {
     'test_all_classification_RVC_TIV_remove'
 }
 
-results_df = extract_experiment_data(exp_dir,experiments_to_check)
+results_df = extract_experiment_data(exp_dir, experiments_to_check)
 
 classification_table(results_df)
 # %%
@@ -85,7 +135,73 @@ experiments_to_check = {
     'test_all_classification_RVC_TIV_remove_25'
 }
 
-results_df = extract_experiment_data(exp_dir,experiments_to_check)
+results_df = extract_experiment_data(exp_dir, experiments_to_check)
 
 classification_table(results_df)
+# %%
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_all_classification_age'
+}
+
+results_df = extract_experiment_data(exp_dir, experiments_to_check)
+
+classification_table(results_df)
+# %%
+
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_classification_ADNI'
+}
+
+results_df = extract_experiment_data(exp_dir, experiments_to_check, False)
+
+classification_table(results_df)
+# %%
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_classification_ADNI_diagnosis_svm'
+}
+
+results_df = extract_experiment_data(exp_dir, experiments_to_check, False)
+
+classification_table(results_df)
+
+# %%
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_classification_ADNI_gender_svm'
+}
+
+results_df = extract_experiment_data(exp_dir, experiments_to_check, False)
+
+classification_table(results_df, stats=["acc"])
+# %%
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_classification_ADNI_FA_svm'
+}
+
+results_df = extract_experiment_data(exp_dir, experiments_to_check, False)
+
+classification_table(results_df, stats=["acc"])
+# %%
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_classification_ADNI_FA_svm'
+}
+
+results_df = extract_experiment_data(exp_dir, experiments_to_check, False)
+
+classification_table(results_df, stats=["auc"])
+# %%
+
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification"
+experiments_to_check = {
+    'test_classification_ADNI_WM_svm'
+}
+
+results_df = extract_experiment_data(exp_dir, experiments_to_check, False)
+
+classification_table(results_df, stats=["auc"])
 # %%
--- 3_check_results/plot_results_oos.py
+++ 3_check_results/plot_results_oos.py
@@ -10,8 +10,8 @@ __file__ = '/home/nnieto/Nico/Harmonization/harmonize_project/3_check_results/pl
 to_append = Path(__file__).resolve().parent.parent.as_posix()
 sys.path.append(to_append)
 
-from lib.utils import table_generation, plot_barplot, extract_experiment_data
-
+from lib.utils import table_generation, plot_barplot, extract_experiment_data, plot_barplot_classification, extract_experiment_data_oos
+#%%
 exp_dir = "/home/nnieto/Nico/Harmonization/results_r8"
 
 experiments_to_check = {'test_all_big_oos_ID1000_r8'}
@@ -29,7 +29,7 @@ results_exp_1000["site"] = "1000Gehirne"
 experiments_to_check = {'test_all_big_oos_eNKI_r8'}
 results_exp_eNKI = extract_experiment_data(exp_dir,experiments_to_check)
 results_exp_eNKI["site"] = "eNKI"
-
+ 
 results_df = pd.concat([results_exp_ID,results_exp_CamCAN,results_exp_1000,results_exp_eNKI])
 
 plot_barplot(results_df, False)
@@ -41,6 +41,78 @@ import seaborn as sns
 sns.color_palette("pastel")
 import matplotlib.pyplot as plt
 
+results_df["y_diff"] = results_df["y_true"]-results_df["y_pred"]
+
+mask1 = results_df["harmonize_mode"] == "pretend_nosite" 
+mask2 = results_df["harmonize_mode"] == "none" 
+
+results_df_2plot = results_df[mask1+mask2]
+
+
+g = sns.catplot(
+    data=results_df_2plot, kind= "boxen",
+    x="site", y="y_diff", hue="harmonize_mode", 
+    height=6
+)
+g.set_axis_labels("", "Prediction difference")
+g.legend.set_title("OOS experiment R8")
+plt.grid(alpha=0.5,axis="y", c="black")
+# %% CLASSIFICATION OOS
+
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification/oos/"
+
+
+experiments_to_check = {'test_all_big_oos_ID1000_classification'}
+results_exp_ID = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_ID["site"] = "ID1000"
+
+experiments_to_check = {'test_all_big_oos_CamCAN_classification'}
+results_exp_CamCAN = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_CamCAN["site"] = "CamCAN"
+
+experiments_to_check = {'test_all_big_oos_1000Gehirne_classification'}
+results_exp_1000 = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_1000["site"] = "1000Gehirne"
+
+experiments_to_check = {'test_all_big_oos_eNKI_classification'}
+results_exp_eNKI = extract_experiment_data(exp_dir,experiments_to_check)
+results_exp_eNKI["site"] = "eNKI"
+
+results_df = pd.concat([results_exp_CamCAN,results_exp_1000,results_exp_eNKI])
+
+plot_barplot_classification(results_df, False)
+table = table_generation(results_df, ["ACC"])
+print(table)
+
+
+# %% CLASSIFICATION OOS AGE
+exp_dir = "/home/nnieto/Nico/Harmonization/results_classification/oos/"
+
+
+experiments_to_check = {'test_all_big_oos_CamCAN_age_classification'}
+results_exp_CamCAN = extract_experiment_data_oos(exp_dir,experiments_to_check)
+results_exp_CamCAN["site"] = "CamCAN"
+
+experiments_to_check = {'test_all_big_oos_1000Gehirne_age_classification'}
+results_exp_1000 = extract_experiment_data_oos(exp_dir,experiments_to_check)
+results_exp_1000["site"] = "1000Gehirne"
+
+experiments_to_check = {'test_all_big_oos_eNKI_age_classification'}
+results_exp_eNKI = extract_experiment_data_oos(exp_dir,experiments_to_check)
+results_exp_eNKI["site"] = "eNKI"
+
+results_df = pd.concat([results_exp_ID,results_exp_CamCAN,results_exp_1000,results_exp_eNKI])
+
+plot_barplot_classification(results_df, False)
+table = table_generation(results_df, ["ACC"])
+print(table)
+
+# %%import seaborn as sns
+
+#%%
+import seaborn as sns
+sns.color_palette("pastel")
+import matplotlib.pyplot as plt
 results_df["y_diff"] = np.abs(results_df["y_true"]-results_df["y_pred"])
 
 g = sns.catplot(
@@ -51,4 +123,5 @@ g = sns.catplot(
 g.set_axis_labels("", "Prediction difference")
 g.legend.set_title("OOS experiment R8")
 plt.grid(alpha=0.5,axis="y", c="black")
+
 # %%
